87c3e6210f31433dfccd5e6d38aff0d5e12c9b7e
ArtemTomilov13 <113232186+ArtemTomilov13@users.noreply.github.com>
2023-07-15 17:44:15 +0300

    model

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,44 @@
+import Controller.ControllerAnimalNursery;
+import Model.*;
+import Model.Template.*;
+import View.*;
+
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        FileOperator fileOperator =
+                new FileOperator("Dog", "Cat" ,"Hamster",
+                        "Horse","Donkey", "Camel");
+
+        TemplateForCat templateForCat = new TemplateForCat();
+        TemplateForDog templateForDog = new TemplateForDog();
+        TemplateForHamster templateForHamster = new TemplateForHamster();
+        TemplateForHorse templateForHorse = new TemplateForHorse();
+        TemplateForDonkey templateForDonkey = new TemplateForDonkey();
+        TemplateForCamel templateForCame = new TemplateForCamel();
+
+        AnimalNursery animalNursery = new AnimalNursery(fileOperator,
+                templateForDog,
+                templateForCat,
+                templateForHamster,
+                templateForHorse,
+                templateForDonkey,
+                templateForCame);
+
+        ControllerAnimalNursery controllerAnimalNursery = new ControllerAnimalNursery(animalNursery);
+
+        ValidationFIO validationFIO = new ValidationFIO();
+        ValidationGender validationGender = new ValidationGender();
+        ValidationData validationData = new ValidationData();
+        ValidationNamber validationNamber = new ValidationNamber();
+
+        ViewAnimalNursery viewAnimalNursery = new ViewAnimalNursery(controllerAnimalNursery,
+                validationFIO,
+                validationGender,
+                validationData,
+                validationNamber);
+
+        viewAnimalNursery.menu();
     }
 }
